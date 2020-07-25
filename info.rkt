d393649f067f47aef00c1d997171fb4f396ad44b
Ryan Jung <ryan.matthew.jung@gmail.com>
2020-07-24 20:20:19 -0700

    possibly fixed info

--- info.rkt
+++ info.rkt
@@ -1,4 +1,10 @@
 #lang info
+
+(define drracket-tools '(("tool.rkt")))
+(define drracket-tool-names (list "code-sync"))
+(define drracket-tool-icons '(#f))
+(define primary-file "tool.rkt")
+
 (define collection "code-sync")
 (define deps '("base" "gui-lib" "data-lib" "drracket-plugin-lib" "rfc6455" "net" "web-server-lib"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
@@ -6,3 +12,4 @@
 (define pkg-desc "Fast Code Sharing for DrRacket")
 (define version "0.0.2")
 (define pkg-authors '(jung.ry@northeastern.edu))
+
